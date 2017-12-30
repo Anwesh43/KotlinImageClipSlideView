b317a7a734f0a66e99b27eb0cc64049875778634
Anwesh43 <anweshthecool0@gmail.com>
2017-12-31 03:40:07 +0530

    adding ImageClipSide

--- imageclipslideview/src/main/java/ui/anwesome/com/imageclipslideview/ImageClipSideView.kt
+++ imageclipslideview/src/main/java/ui/anwesome/com/imageclipslideview/ImageClipSideView.kt
@@ -19,4 +19,15 @@ class ImageClipSideView(ctx:Context,var bitmap:Bitmap):View(ctx) {
         }
         return true
     }
+    data class ImageClipSide(var x:Float,var w:Float,var ox:Float = x) {
+        fun draw(canvas:Canvas,paint:Paint,bitmap:Bitmap,scale:Float) {
+            x = ox + w*scale
+            canvas.save()
+            val path = Path()
+            path.addRect(RectF(x,0f,x+w,bitmap.width.toFloat()),Path.Direction.CW)
+            canvas.clipPath(path)
+            canvas.drawBitmap(bitmap,-w*scale,0f,paint)
+            canvas.restore()
+        }
+    }
 }
