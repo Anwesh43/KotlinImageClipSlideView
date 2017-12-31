6ca4877363f9b931f8291a516ebc9333be751fc5
Anwesh43 <anweshthecool0@gmail.com>
2017-12-31 05:37:03 +0530

    using view in actvity

--- app/src/main/java/ui/anwesome/com/kotlinimageclipslideview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinimageclipslideview/MainActivity.kt
@@ -1,12 +1,14 @@
 package ui.anwesome.com.kotlinimageclipslideview
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.imageclipslideview.ImageClipSideView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ImageClipSideView.create(this,BitmapFactory.decodeResource(resources,R.drawable.trphy))
     }
 }
