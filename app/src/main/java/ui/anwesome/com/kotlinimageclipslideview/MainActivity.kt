652dc530cedabb4c0c2cd1886fb54ae9181e74c5
Anwesh43 <anweshthecool0@gmail.com>
2018-01-01 22:06:33 +0530

    calling onUpdateListener

--- app/src/main/java/ui/anwesome/com/kotlinimageclipslideview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinimageclipslideview/MainActivity.kt
@@ -4,11 +4,16 @@ import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import ui.anwesome.com.imageclipslideview.ImageClipSideView
+import ui.anwesome.com.lineindicatorview.LineIndicatorView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ImageClipSideView.create(this,BitmapFactory.decodeResource(resources,R.drawable.trphy))
+        val view = ImageClipSideView.create(this,BitmapFactory.decodeResource(resources,R.drawable.trphy))
+        val lineIndicatorView = LineIndicatorView.create(this)
+        view.setOnUpdateListener {  
+            lineIndicatorView.update(it)
+        }
     }
 }
